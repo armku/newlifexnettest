4cf00b6a3a24faafe83ab0410dff3d7c59127536
Ace <258613763@qq.com>
2018-06-16 22:01:36 +0800

    代码整理

--- EchoTest/Program.cs
+++ EchoTest/Program.cs
@@ -13,13 +13,7 @@ namespace EchoTest
             XTrace.UseConsole();
 
             try
-            {
-                Console.Write("请选择运行模式：1，服务端；2，客户端  ");
-                var ch = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-                if (ch == '1')
-                    TestServer();
-                else
+            {                               
                     TestClient();
             }
             catch (Exception ex)
@@ -33,22 +27,7 @@ namespace EchoTest
 
         static TimerX _timer;
         static NetServer _server;
-        static void TestServer()
-        {
-            // 实例化服务端，指定端口，同时在Tcp/Udp/IPv4/IPv6上监听
-            var svr = new MyNetServer
-            {
-                Port = 1234,
-                Log = XTrace.Log
-            };
-            svr.Start();
-
-            _server = svr;
-
-            // 定时显示性能数据
-            _timer = new TimerX(ShowStat, svr, 100, 1000);
-        }
-
+        
         static void TestClient()
         {
             var uri = new NetUri("tcp://127.0.0.1:1234");
