ec9b4a523dcf21e2f142aec837f5bcb4f9ef6b4e
Ace <258613763@qq.com>
2018-06-16 23:45:22 +0800

    代码整理

--- RpcServerDemo/Program.cs
+++ RpcServerDemo/Program.cs
@@ -15,14 +15,8 @@ namespace RpcTest
             XTrace.UseConsole();
 
             try
-            {
-                Console.Write("请选择运行模式：1，服务端；2，客户端  ");
-                var ch = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-                if (ch == '1')
+            {                
                     TestServer();
-                else
-                    TestClient();
             }
             catch (Exception ex)
             {
@@ -60,50 +54,6 @@ namespace RpcTest
 
             // 定时显示性能数据
             _timer = new TimerX(ShowStat, ns, 100, 1000);
-        }
-
-        static async void TestClient()
-        {
-            var client = new MyClient("tcp://127.0.0.1:1234");
-
-            // 指定编码器
-            client.Encoder = new JsonEncoder();
-            client.EncoderLog = XTrace.Log;
-
-            // 打开原始数据日志
-            var ns = client.Client;
-            ns.Log = XTrace.Log;
-            ns.LogSend = true;
-            ns.LogReceive = true;
-
-            client.Log = XTrace.Log;
-            client.Open();
-
-            // 定时显示性能数据
-            _timer = new TimerX(ShowStat, ns, 100, 1000);
-
-            // 标准服务，Json
-            var n = await client.AddAsync(1245, 3456);
-            XTrace.WriteLine("Add: {0}", n);
-
-            // 高速服务，二进制
-            var buf = "Hello".GetBytes();
-            var pk = await client.RC4Async(buf);
-            XTrace.WriteLine("RC4: {0}", pk.ToHex());
-
-            // 返回对象
-            var user = await client.FindUserAsync(123, true);
-            XTrace.WriteLine("FindUser: ID={0} Name={1} Enable={2} CreateTime={3}", user.ID, user.Name, user.Enable, user.CreateTime);
-
-            // 拦截异常
-            try
-            {
-                user = await client.FindUserAsync(123, true);
-            }
-            catch (ApiException ex)
-            {
-                XTrace.WriteLine("FindUser出错，错误码={0}，内容={1}", ex.Code, ex.Message);
-            }
         }
 
         static void ShowStat(Object state)
